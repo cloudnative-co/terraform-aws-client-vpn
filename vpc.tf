a04d6b7c42aa67bd7d3fd32c746002717d812d88
Isobe Kazuhiko <muramasa64@gmail.com>
2023-04-04 22:24:15 +0900

    fix: gateway_id to nat_gateway_id

--- vpc.tf
+++ vpc.tf
@@ -50,8 +50,8 @@ resource "aws_route_table" "bastion-public" {
 resource "aws_route_table" "bastion-private" {
   vpc_id = aws_vpc.bastion.id
   route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.bastion.id
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.bastion.id
   }
   tags = {
     Name = "${var.name}-private-route-table"
