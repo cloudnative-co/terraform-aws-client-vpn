561eea6a66eae42e7e40b61093a79a90099ab004
Isobe Kazuhiko <muramasa64@gmail.com>
2023-04-04 22:52:52 +0900

    update: NAT Gateway configuration to vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -94,3 +94,17 @@ resource "aws_security_group" "bastion" {
     self             = false
   }]
 }
+
+# NAT Gateway
+resource "aws_nat_gateway" "bastion" {
+  allocation_id = var.nat-elastic-ip-id
+  subnet_id     = aws_subnet.bastion-public.id
+
+  tags = {
+    Name = "${var.name}-nat-gateway"
+  }
+
+  depends_on = [
+    aws_internet_gateway.bastion
+  ]
+}
